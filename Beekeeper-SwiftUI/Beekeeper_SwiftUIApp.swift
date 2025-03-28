6f5ee74297503718382d2b417fb9b9b733d1f821
Paweł Rudnik <pawelrudnik3@gmail.com>
2025-03-28 13:50:59 +0100

    Add connecting Firebase when app starts up

--- Beekeeper-SwiftUI/Beekeeper_SwiftUIApp.swift
+++ Beekeeper-SwiftUI/Beekeeper_SwiftUIApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct Beekeeper_SwiftUIApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("Configured Firebase!")
+        return true
     }
 }
