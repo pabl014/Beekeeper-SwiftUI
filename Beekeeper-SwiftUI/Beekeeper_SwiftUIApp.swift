06e4b349448bef5da18d33f2dc0ef8216ceac28c
Paweł Rudnik <pawelrudnik3@gmail.com>
2025-03-28 12:17:19 +0100

    Initial Commit

---
+++ Beekeeper-SwiftUI/Beekeeper_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beekeeper_SwiftUIApp.swift
+//  Beekeeper-SwiftUI
+//
+//  Created by Paweł Rudnik on 28/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Beekeeper_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beekeeper-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Beekeeper-SwiftUI
+//
+//  Created by Paweł Rudnik on 28/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
